c31a2fcf264dc4195be8eed0879b6c5004ec494d
Ozgur <ozgurozvaris@hotmail.com>
2022-03-12 16:23:49 +0000

    revalidate next js

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import path from "path";
+import fs from "fs/promises";
+
+const HomePage = (props) => {
+  const { products } = props;
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export const getStaticProps = async () => {
+  console.log("(Re-)Generating");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: { products: data.products },
+    revalidate: 10,
+  };
+};
+
+export default HomePage;
